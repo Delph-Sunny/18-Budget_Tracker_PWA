26fedb1c961c650727f81c91c3c8d557d277ca09
Delph <delph_teb@hotmail.com>
2021-03-11 01:54:34 -0500

    add basic service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,90 @@
 console.log("Hi from your service-worker.js file!");
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/assets/css/style.css",
+  "/assets/js/index.js",
+  "/assets/icons/icon-192x192.png",
+  "/assets/images/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+];
+
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
+
+// install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches
+      .open(STATIC_CACHE)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// activate
+self.addEventListener("activate", event => {
+  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames => {
+        // return array of cache names that are old to delete
+        return cacheNames.filter(
+          cacheName => !currentCaches.includes(cacheName)
+        );
+      })
+      .then(cachesToDelete => {
+        return Promise.all(
+          cachesToDelete.map(cacheToDelete => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+
+// fetch
+self.addEventListener("fetch", event => {
+  if (
+    event.request.method !== "GET" ||
+        !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  if (event.request.url.includes("/api")) {
+    event.respondWith(
+      caches.open(RUNTIME_CACHE).then(cache =>
+        fetch(event.request)
+          .then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request))
+      )
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return caches
+        .open(RUNTIME_CACHE)
+        .then(cache =>
+          fetch(event.request).then(response =>
+            cache.put(event.request, response.clone()).then(() => response)
+          )
+        );
+    })
+  );
+});
